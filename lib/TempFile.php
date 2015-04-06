d46a94cfd0afd25b033c3a5ff63561954c3823f1
Sam Williams <samuel.williams8@gmail.com>
2015-04-07 08:34:24 +1000

    Added a class to handle temporary files.

---
+++ lib/TempFile.php
@@ -0,0 +1,70 @@
+<?php
+/*
+ * This file is part of badcow-common.
+ *
+ * (c) Samuel Williams <sam@badcow.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Badcow\Common;
+
+class TempFile
+{
+    private $tmpFile;
+    private $tmpDir;
+    private $isDeleted = false;
+
+    public function __construct($prefix = '')
+    {
+        $this->tmpDir = sys_get_temp_dir();
+        $this->tmpFile = tempnam($this->tmpDir, $prefix);
+    }
+
+    /**
+     * @param $string
+     * @param string $mode
+     */
+    public function write($string, $mode = 'w')
+    {
+        $handle = fopen($this->tmpFile, $mode);
+        fwrite($handle, $string);
+        fclose($handle);
+    }
+
+    /**
+     * @return string
+     */
+    public function read()
+    {
+        return file_get_contents($this->tmpFile);
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath()
+    {
+        return $this->tmpFile;
+    }
+
+    /**
+     * Delete the file
+     */
+    public function delete()
+    {
+        if ($this->isDeleted) {
+            return;
+        }
+
+        unlink($this->tmpFile);
+        $this->isDeleted = true;
+    }
+
+    public function __destruct()
+    {
+        $this->delete();
+    }
+}
+ 
---
+++ lib/Tests/TempFileTest.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ * This file is part of badcow-common.
+ *
+ * (c) Samuel Williams <sam@badcow.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Badcow\Common\Tests;
+
+use Badcow\Common\TempFile;
+
+class TempFileTest extends \PHPUnit_Framework_TestCase
+{
+    const PREFIX = 'badcow_common_test_';
+
+    private $twoCities = <<<TEXT
+It was the best of times,
+it was the worst of times,
+it was the age of wisdom,
+it was the age of foolishness...
+TEXT;
+
+    public function testConstructor()
+    {
+        $tmpFile = new TempFile(static::PREFIX);
+        $this->assertTrue(file_exists($tmpFile->getPath()));
+    }
+
+    public function testWrite()
+    {
+        $tmpFile = new TempFile(static::PREFIX);
+        $tmpFile->write($this->twoCities);
+        $this->assertEquals($this->twoCities, file_get_contents($tmpFile->getPath()));
+    }
+
+    public function testRead()
+    {
+        $tmpFile = new TempFile(static::PREFIX);
+        file_put_contents($tmpFile->getPath(), $this->twoCities);
+        $this->assertEquals($this->twoCities, $tmpFile->read());
+    }
+
+    public function testDelete()
+    {
+        $tmpFile = new TempFile(static::PREFIX);
+        $this->assertTrue(file_exists($tmpFile->getPath()));
+        $tmpFile->delete();
+        $this->assertFalse(file_exists($tmpFile->getPath()));
+    }
+}
+ 
